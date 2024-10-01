a6ed84fc3021ce25c08b4f7a662232055c0fdd58
ggcr <ggcristian@icloud.com>
2024-10-01 13:46:17 +0200

    `utils/readFile` function now removes punctuation and special chars.

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,8 @@ def readFile(path: str) -> str:
         with open(path, 'r') as fd:
             res = fd.read()
             # remove non-alpha chars from word
-            res = re.sub('[^a-zA-Z0-9]*$', '', res)
+            res = re.sub('[^a-zA-Z0-9]+', ' ', res).strip() # punctuations
+            res = re.sub('[^a-zA-Z0-9]*$', '', res).lower() # special chars at the end
         return res
     except FileNotFoundError:
         print(f"File with path {path} was not found.")
