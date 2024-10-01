faf71c7f7ff985332ecd1d1e23d6c6b3b42c8740
ggcr <ggcristian@icloud.com>
2024-10-01 13:47:44 +0200

    Confirmed that writting is robust for concurency (no race conds). Altered the flag of the open fd python method to 'a' to not do overwritting.

--- src/driver.py
+++ src/driver.py
@@ -5,54 +5,77 @@ from src import utils
 # Naïve approach: spawn a worker for each file.
 
 BUCKETS_PARENT_PATH = "files/intermediate/"
+os.makedirs(BUCKETS_PARENT_PATH, exist_ok=True)
 
 class Worker:
-    def __init__(self, n: int, M: int, FILE_NAME: str):
+    def __init__(self, n: int):
         self.n = n
-        self.M = M
-        self.file_name = FILE_NAME
 
-    def map(self):
+    def map(self, M: int, FILENAME: str):
         # read file
-        contents = utils.readFile(self.file_name).split(' ')
-        buckets = {m: [] for m in range(0, self.M)}
+        contents = utils.readFile(FILENAME).split(' ')
+        buckets = {}
         for word in contents:
-            buckets[ord(word[0]) % self.M].append(word)
+            b_id = ord(word[0]) % M
+            buckets[b_id] = buckets.get(b_id, []) + [word]
+
         # write intermediary result to bucket
-        os.makedirs(BUCKETS_PARENT_PATH, exist_ok=True)
-        # (TODO): Serialize with pickle if needed.
+        # (TODO): Serialize with pickle (more efficent)?
+        # note: there are not race conditions because the bucket is unique for each map worker node
         for bucket_num, v in buckets.items():
             bucket_path = os.path.join(BUCKETS_PARENT_PATH, f"m-{self.n}-{bucket_num}")
-            with open(bucket_path, 'w') as fd:
+            with open(bucket_path, 'a') as fd:
                 [fd.write(f"{word}\n") for word in v]
 
     def reduce(self):
         pass
         
 
-def dummy_worker(n: int, M: int, filename: str):
+def map_worker(n: int, M: int, filename: str) -> None:
     print(f"[ID {n}, M {M}] FILE {filename}")
-    w = Worker(n, M, filename)
-    w.map()
+    w = Worker(n)
+    w.map(M, filename)
+
+def reduce_worker(m: int, buckets: list[str]):
+    print(f"[ID {m}] {buckets}")
+    w = Worker(m)
 
 def spawn_workers(N: int, M: int) -> None:
+    # MAP PHASE
+    print(f"Starting {N} map workers")
     FILES = ["inputs/simple_test_1.txt", "inputs/simple_test_2.txt", "inputs/simple_test_3.txt",]
+    N = len(FILES) # provisional
     threads = []
-    for i in range(0, N):
+    for n in range(0, N):
         t = multiprocessing.Process(
-            target=dummy_worker,
-            args=(i, M, FILES[i])
+            target=map_worker,
+            args=(n, M, FILES[n])
         )
         threads.append(t)
         t.start()
 
     for t in threads:
         t.join()
-    print("Done")
+    print("Map phase done!\n")
+
+    # REDUCE PHASE
+    print(f"Starting {M} reduce workers")
+    avail_buckets = [os.path.join(BUCKETS_PARENT_PATH, f"mr-{n}-{m}") for n in range(0, N) for m in range(0, M)]
+    for m in range(0, M):
+        avail_buckets = [os.path.join(BUCKETS_PARENT_PATH, f"mr-{n}-{m}") for n in range(0, N)]
+        t = multiprocessing.Process(
+            target=reduce_worker,
+            args=(m, avail_buckets, )
+        )
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+    print("Reduce phase done!\n")
     
 if __name__ == '__main__':
     N = 3 # map tasks
     M = 5 # reduce tasks
 
-    print(f"Starting {N} workers")
     spawn_workers(N, M)
