0acddb645ce1eacafa0ac2b8406b45a84c9baef3
ggcr <ggcristian@icloud.com>
2024-10-03 09:21:10 +0200

    Overwritting control on new runs to avoid stacking results. Also implemented the final reduce of the `/files/out` buckets.

--- src/driver.py
+++ src/driver.py
@@ -7,6 +7,8 @@ from src import utils
 BUCKETS_PARENT_PATH = "files/intermediate/"
 os.makedirs(BUCKETS_PARENT_PATH, exist_ok=True)
 
+REDUCE_PARENT_PATH = "files/out/"
+os.makedirs(REDUCE_PARENT_PATH, exist_ok=True)
 
 class Worker:
     def __init__(self, n: int):
@@ -21,32 +23,36 @@ class Worker:
             buckets[b_id] = buckets.get(b_id, []) + [word]
 
         # create bucket files
-        BUCKETS_PARENT_PATH = "files/intermediate/"
-        os.makedirs(BUCKETS_PARENT_PATH, exist_ok=True)
-        [open(os.path.join(BUCKETS_PARENT_PATH, f"mr-{n}-{m}"), 'a').close() for m in range(0, M) for n in range(0, self.n)]
+        # BUCKETS_PARENT_PATH = "files/intermediate/"
+        # os.makedirs(BUCKETS_PARENT_PATH, exist_ok=True)
+        # [open(os.path.join(BUCKETS_PARENT_PATH, f"mr-{n}-{m}"), 'a').close() for m in range(0, M) for n in range(0, self.n)]
 
         # write intermediate result to bucket
         # (TODO): Serialize with pickle (more efficent)?
         # note: there are not race conditions because the bucket is unique for each map worker node
+
+        # This write flag will overwrite previous executions runs results
+        overwrite = {bucket_num: True for bucket_num in set(buckets.keys())} 
         for bucket_num, v in buckets.items():
             bucket_name = f"mr-{self.n}-{bucket_num}"
             bucket_path = os.path.join(BUCKETS_PARENT_PATH, bucket_name)
-            with open(bucket_path, 'a') as fd:
+            with open(bucket_path, 'w' if overwrite[bucket_num] else 'a') as fd:
                 [fd.write(f"{word}\n") for word in v]
+            overwrite[bucket_num] = False
+
 
     def reduce(self, m: int, buckets: list[str]):
         # count words from each reduce bucket
         res = {}
         for bucket in buckets:
             data = []
-            with open(bucket, 'r') as fd:
-                data = fd.read().splitlines() 
-            for word in data:
-                res[word] = res.get(word, 0) + 1
+            if os.path.exists(bucket):
+                with open(bucket, 'r') as fd:
+                    data = fd.read().splitlines() 
+                for word in data:
+                    res[word] = res.get(word, 0) + 1
         print(res)
         # output to file
-        REDUCE_PARENT_PATH = "files/out/"
-        os.makedirs(REDUCE_PARENT_PATH, exist_ok=True)
         out_path = os.path.join(REDUCE_PARENT_PATH, f"out-{m}")
         with open(out_path, 'w') as fd:
             [fd.write(f"{k} {v}\n") for k, v in res.items()]
@@ -93,9 +99,23 @@ def spawn_workers(N: int, M: int) -> None:
     for t in threads:
         t.join()
     print("Reduce phase done!\n")
+
+def accumulate_output(M: int):
+    count = {}
+    for m in range(0, M):
+        out_bucket = os.path.join(REDUCE_PARENT_PATH, f"out-{m}")
+        if os.path.exists(out_bucket):
+            with open(out_bucket, 'r') as fd:
+                bufcont = fd.read().splitlines()
+                for record in bufcont:
+                    k, v = record.split(' ')
+                    count[k] = count.get(k, 0) + int(v)
+    return count
     
 if __name__ == '__main__':
     N = 3 # map tasks
     M = 5 # reduce tasks
 
     spawn_workers(N, M)
+    final_count = accumulate_output(M)
+    print(final_count)
